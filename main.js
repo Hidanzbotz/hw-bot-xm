be3b2b060a843a6e01abd48bfbd28a1606637870
Hidanzbotz <91834504+Hidanzbotz@users.noreply.github.com>
2021-12-13 23:18:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ nocache('./index.js', module => console.log(`${module} is now updated!`))
 
 const starts = async (Haikal = new WAConnection()) => {
     Haikal.logger.level = 'warn'
-    Haikal.version = [2, 2140, 12]
+    Haikal.version = [2, 2143, 3]
     Haikal.browserDescription = [ 'HW MODS WA', 'Chrome', '3.0' ]
     console.log(banner.string)
     Haikal.on('qr', () => {
